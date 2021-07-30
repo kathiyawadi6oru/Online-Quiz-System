ce86871ca8c95253a5b73855b393a0c2af6f10e8
Mayank Vachhani <mayankvachhani91@gmail.com>
2021-07-30 10:14:43 +0530

    Delete admin directory

--- admin/connection.php
+++
@@ -1,8 +0,0 @@
-<?php	
-    $con = mysqli_connect('localhost','root','','exam');
-     
-    if(!$con)
-    {
-        die('connect error:'.mysqli_connect_errno().mysqli_connect_errno());
-    }
-?>
--- admin/demo.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo strripos("yash modi","ash");
-?>
